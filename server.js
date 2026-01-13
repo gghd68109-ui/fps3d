ef6b1f38f32e269c644043fe933e4ce55c8fb21d
gghd68109-ui <gghd68109-ui@users.noreply.github.com>
2026-01-14 02:34:23 +0300

    Working multiplayer8

--- server.js
+++ server.js
@@ -1,33 +1,63 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const path = require("path");
+const WebSocket = require("ws");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+/* -------------------- */
+/*   PUBLIC KLASÖRÜ     */
+/* -------------------- */
+app.use(express.static(path.join(__dirname, "public")));
 
-const players = {};
-
-io.on("connection", socket => {
-    players[socket.id] = { x: 0, y: 1, z: 0 };
-
-    socket.emit("players", players);
-
-    socket.on("move", data => {
-        players[socket.id] = data;
-        socket.broadcast.emit("player", { id: socket.id, data });
-    });
-
-    socket.on("disconnect", () => {
-        delete players[socket.id];
-        io.emit("remove", socket.id);
-    });
+/* Ana sayfa */
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("Running on", PORT);
+/* -------------------- */
+/*   HTTP SERVER        */
+/* -------------------- */
+const server = app.listen(PORT, () => {
+  console.log("HTTP Server running on port", PORT);
 });
+
+/* -------------------- */
+/*   WEBSOCKET SERVER   */
+/* -------------------- */
+const wss = new WebSocket.Server({ server });
+
+let players = {};
+
+wss.on("connection", ws => {
+  const id = Math.random().toString(36).substr(2, 9);
+  players[id] = { x: 0, y: 0, z: 0 };
+
+  console.log("Player connected:", id);
+
+  ws.on("message", msg => {
+    try {
+      const data = JSON.parse(msg);
+      if (data.type === "move") {
+        players[id] = data.pos;
+      }
+    } catch {}
+  });
+
+  ws.on("close", () => {
+    delete players[id];
+    console.log("Player disconnected:", id);
+  });
+});
+
+/* Oyuncuları yayınla */
+setInterval(() => {
+  const data = JSON.stringify({ type: "players", players });
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+}, 50);
+
 
